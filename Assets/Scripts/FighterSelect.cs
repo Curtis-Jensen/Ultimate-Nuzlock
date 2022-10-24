121ac0a6758b17203a444337a702b675dfd4d573
Curtis-Jensen <me@curtisjensen.com>
2022-10-24 13:12:21 -0600

    Made some more scenes.  Put in a check for the infnite while loop so it doesn't crash everything

--- Assets/Scripts/FighterSelect.cs
+++ Assets/Scripts/FighterSelect.cs
@@ -13,7 +13,10 @@ public class FighterSelect : MonoBehaviour
      * 2 Checks that the next opponent being chosen is not already a friend
      * It does this by chosing a random fighter and making sure it's not already a friend
      * 
-     * 3 Once the fighter has been chosen, assume it's a friend, and then update the text to show who it is
+     * 3 Once the fighter has been chosen, assume it's going to be beaten and become a friend,
+     * and then update the text to show who it is
+     * 
+     * 4 If there are only 3 fighters left, show the squad strike
      */
     public string ChooseFighter()
     {
@@ -22,18 +25,37 @@ public class FighterSelect : MonoBehaviour
         System.Random randomNumberGenerator = new System.Random();
         Fighter chosenFighter;
         int randomNumber;
+        int remainingFighters = fighters.Length;
         #endregion//1
 
+        int i = 0;
         do//2
         {
-            randomNumber = randomNumberGenerator.Next(1, 12);
+            randomNumber = randomNumberGenerator.Next(0, fighters.Length);
             chosenFighter = fighters[randomNumber];
+
+            i++;
+            if (i > 1000) throw new UnityException("Got stuck in an infinite while loop");
         } while (chosenFighter.status != Fighter.Status.Enemy) ;
 
         fighters[randomNumber].status = Fighter.Status.Friend;//3
         fighterText.text = chosenFighter.name;
 
+        remainingFighters--;
+        if (remainingFighters == 3) fighterText.text = ArrangeSquadStrike();//4
+
         return chosenFighter.name;
+    }
+
+    string ArrangeSquadStrike()
+    {
+        string fullList = "";
+        foreach(Fighter fighter in fighters)
+        {
+            fullList += fighter.name + "\n";
+        }
+
+        return fullList;
     }
 }
 
@@ -41,6 +63,6 @@ public class FighterSelect : MonoBehaviour
 public struct Fighter
 {
     public string name;
-    public enum Status {Enemy, Friend, Dead };
+    public enum Status {Enemy, Friend, Dead};
     public Status status;
 }
